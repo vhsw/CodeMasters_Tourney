e56e5aefe3f82bc09940533b1a066787d3272654
Alexey Dubrov <nevermind1025@gmail.com>
2018-05-21 19:40:30 +0300

    mo

--- Python 3/removeDuplicateCharacters.py
+++ Python 3/removeDuplicateCharacters.py
@@ -3,3 +3,18 @@
 
 def removeDuplicateCharacters(str):
     return ''.join(l for l in str if str.count(l) == 1)
+
+def removeDuplicateCharacters(inputStr):
+    charCount = {}
+    newInputStr = []
+    for i in range(len(inputStr)):
+        char =  inputStr[i]
+        if char in charCount:
+            charCount[char] += 1
+        else:
+            charCount[char] = 1
+    for i in range(len(inputStr)):
+        if charCount[inputStr[i]] == 1:
+            newInputStr.append(inputStr[i])
+    return ''.join(newInputStr)
+    
