f5dd2ca3f462cee6aca861438b3202c9188632f7
Alexey Dubrov <vhsw@users.noreply.github.com>
2018-05-22 06:41:17 +0300

    Update validTime.py

--- Python 3/validTime.py
+++ Python 3/validTime.py
@@ -6,7 +6,7 @@ def validTime(time):
     def charToInt(symbol):
         return ord(symbol) - ord('0')
 
-    hours = charToInt(time[0]) + charToInt(time[1])
+    hours = charToInt(time[0]) *10 + charToInt(time[1])
     minutes = charToInt(time[3]) * 10 + charToInt(time[4])
 
     if hours < 24 and minutes < 60:
